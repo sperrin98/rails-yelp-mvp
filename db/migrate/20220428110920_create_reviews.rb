de1e15a713fd78f052aab55b1ab2aafb18d55efb
stanleyperrin <stanperrin98@gmail.com>
2022-04-28 20:05:24 +0100

    yelp

--- db/migrate/20220428110920_create_reviews.rb
+++ db/migrate/20220428110920_create_reviews.rb
@@ -1,8 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      t.text :content
       t.integer :rating
-      t.string :content
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_26_172509) do
+ActiveRecord::Schema.define(version: 2022_04_28_110920) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.text "content"
     t.integer "rating"
-    t.string "content"
+    t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
